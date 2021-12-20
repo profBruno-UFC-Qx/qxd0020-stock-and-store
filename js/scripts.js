6631be5af8966c8a7d01cc6e5b260ddd9beb01f2
Bruno Mateus <brunomateus@gmail.com>
2021-12-20 20:08:19 -0300

    Stock is working

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,26 @@
 const listProduct = {
     data() {
         const productsArray = [];
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 3; i++) {
             productsArray.push({
                 name: faker.commerce.productName(),
                 description: faker.commerce.productDescription(),
                 price: faker.commerce.price(),
                 sale: Math.random() > 0.5,
+                quantity: 3
             });
         }
         return {
             products: productsArray
         }
+    },
+
+    methods: {
+        remove(i) {
+            this.products.splice(i, 1);
+        }
     }
+
 }
 
 Vue.createApp(listProduct).mount('#app');
