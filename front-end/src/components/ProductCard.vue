3a525df2c9005bb614ae7772d44f129d693a8a50
Bruno Mateus <brunomateus@gmail.com>
2022-01-19 22:22:32 -0300

    Adding a directive to bind css class

--- front-end/src/components/ProductCard.vue
+++ front-end/src/components/ProductCard.vue
@@ -19,7 +19,7 @@ const priceWithDiscount = computed((): string  => (props.price - (props.discount
 <template>
     <div class="col mb-5">
         <div class="card h-100">
-            <div v-if="sale" class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+            <div v-if="sale" :class="{ 'bg-danger': sale}" class="badge text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
             <!-- Product image-->
             <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
             <!-- Product details-->
@@ -29,12 +29,11 @@ const priceWithDiscount = computed((): string  => (props.price - (props.discount
                     <h5 class="fw-bolder">{{name}}</h5>
                     <!-- Product reviews-->
                     <div v-if="rating" class="d-flex justify-content-center small text-warning mb-2">
-                        <div v-for="i in rating" class="bi-star-fill"></div>
+                        <div v-for="i in rating" :key="i" class="bi-star-fill"></div>
                     </div>
                     <!-- Product price-->
-                    <span v-if="sale" class="text-muted text-decoration-line-through">R${{price}}</span>
-                    <span v-if="sale"> R${{priceWithDiscount}}</span>
-                    <span v-else>R${{price}}</span>
+                    <span :class="{ 'text-decoration-line-through' : sale, 'text-muted': sale}">R${{price}}</span>
+                    <span v-if="sale"> - {{priceWithDiscount}}</span>
                 </div>
             </div>
             <!-- Product actions-->
